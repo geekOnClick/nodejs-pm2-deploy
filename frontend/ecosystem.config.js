f9902655286cfca78a5be2574437f2205011e383
geekOnClick <kozloww1508@yandex.ru>
2024-12-10 21:55:14 +0300

    4

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -8,13 +8,12 @@ module.exports = {
 
   deploy: {
     production: {
-      key: '../praktikum-ubuntu.pem',
+      key: 'C:\\\\Users\\\\Alex\\\\.ssh\\\\praktikum-ubuntu.pem',
       user: process.env.REMOTE_USER,
       host: process.env.REMOTE_HOST,
       ref: process.env.REF,
       repo: process.env.REPO,
       path: process.env.REMOTE_PATH,
-      // 'pre-deploy-local': `scp ./.env ${process.env.REMOTE_USER}@${process.env.REMOTE_HOST}:${process.env.REMOTE_PATH}`,
       'post-deploy': 'cd frontend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
