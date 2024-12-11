7f17202220dc139defe76dade29e822371bdf9ce
geekOnClick <kozloww1508@yandex.ru>
2024-12-11 19:13:20 +0300

    9

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -139,6 +139,6 @@ class Api {
   }
   }
   // Замените на адрес вашего бэкенда
-  const api = new Api('api.nomorepartiesco.ru');
+  const api = new Api('http://geekonclick-api.nomorepartiesco.ru');
 
   export default api;
